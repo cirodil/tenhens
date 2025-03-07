e55c940ce1bc03c69a05ffd7a10ee6393b83f458
cirodil <cirodil@gmail.com>
2025-03-07 20:48:34 +0300

    fix ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,14 +10,14 @@ function App() {
 
   // Загрузка записей
   useEffect(() => {
-    fetch(`http://localhost:8000/records/${userId}`)
+    fetch(`http://176.123.167.178/records/${userId}`)
       .then((response) => response.json())
       .then((data) => setRecords(data.records));
   }, []);
 
   // Добавление записи
   const addRecord = () => {
-    fetch("http://localhost:8000/records/", {
+    fetch("http://176.123.167.178/records/", {
       mode: "no-cors",
       method: "POST",
       headers: {
@@ -37,7 +37,7 @@ function App() {
         setDate("");
         setNotes("");
         // Обновляем список записей
-        fetch(`http://localhost:8000/records/${userId}`, { mode: "no-cors" })
+        fetch(`http://176.123.167.178/records/${userId}`, { mode: "no-cors" })
           .then((response) => response.json())
           .then((data) => setRecords(data.records));
       });
